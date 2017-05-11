e3a1098390162e2befae958aab0e69c394a49e2f
TadaoYamaoka <tadaoyamaoka@gmail.com>
2017-05-11 23:10:35 +0900

    Wide ResNet

--- train/train.py
+++ train/train.py
@@ -101,24 +101,24 @@ PIECE_MOVE_DIRECTION_LABEL = [
 ]
 
 k = 256
+dropout_ratio = 0.1
 class MyChain(Chain):
     def __init__(self):
         super(MyChain, self).__init__(
-            l1_1=L.Convolution2D(in_channels = None, out_channels = k, ksize = 3, pad = 1),
-            l1_2=L.Convolution2D(in_channels = None, out_channels = k, ksize = 1), # pieces_in_hand
-            l2=L.Convolution2D(in_channels = k, out_channels = k, ksize = 3, pad = 1),
-            l3=L.Convolution2D(in_channels = k, out_channels = k, ksize = 3, pad = 1),
-            l4=L.Convolution2D(in_channels = k, out_channels = k, ksize = 3, pad = 1),
-            l5=L.Convolution2D(in_channels = k, out_channels = k, ksize = 3, pad = 1),
-            l6=L.Convolution2D(in_channels = k, out_channels = k, ksize = 3, pad = 1),
-            l7=L.Convolution2D(in_channels = k, out_channels = k, ksize = 3, pad = 1),
-            l8=L.Convolution2D(in_channels = k, out_channels = k, ksize = 3, pad = 1),
-            l9=L.Convolution2D(in_channels = k, out_channels = k, ksize = 3, pad = 1),
-            l10=L.Convolution2D(in_channels = k, out_channels = k, ksize = 3, pad = 1),
-            l11=L.Convolution2D(in_channels = k, out_channels = k, ksize = 3, pad = 1),
-            l12=L.Convolution2D(in_channels = k, out_channels = k, ksize = 3, pad = 1),
-            l13=L.Convolution2D(in_channels = k, out_channels = MOVE_DIRECTION_LABEL_NUM, ksize = 1, nobias = True),
-            l13_2=L.Bias(shape=(9*9*MOVE_DIRECTION_LABEL_NUM)),
+            l1_1=L.Convolution2D(in_channels = None, out_channels = k, ksize = 3, pad = 1, nobias = True),
+            l1_2=L.Convolution2D(in_channels = None, out_channels = k, ksize = 1, nobias = True), # pieces_in_hand
+            l2=L.Convolution2D(in_channels = k, out_channels = k, ksize = 3, pad = 1, nobias = True),
+            l3=L.Convolution2D(in_channels = k, out_channels = k, ksize = 3, pad = 1, nobias = True),
+            l4=L.Convolution2D(in_channels = k, out_channels = k, ksize = 3, pad = 1, nobias = True),
+            l5=L.Convolution2D(in_channels = k, out_channels = k, ksize = 3, pad = 1, nobias = True),
+            l6=L.Convolution2D(in_channels = k, out_channels = k, ksize = 3, pad = 1, nobias = True),
+            l7=L.Convolution2D(in_channels = k, out_channels = k, ksize = 3, pad = 1, nobias = True),
+            l8=L.Convolution2D(in_channels = k, out_channels = k, ksize = 3, pad = 1, nobias = True),
+            l9=L.Convolution2D(in_channels = k, out_channels = k, ksize = 3, pad = 1, nobias = True),
+            l10=L.Convolution2D(in_channels = k, out_channels = k, ksize = 3, pad = 1, nobias = True),
+            l11=L.Convolution2D(in_channels = k, out_channels = k, ksize = 3, pad = 1, nobias = True),
+            l12=L.Convolution2D(in_channels = k, out_channels = MOVE_DIRECTION_LABEL_NUM, ksize = 1, nobias = True),
+            l12_2=L.Bias(shape=(9*9*MOVE_DIRECTION_LABEL_NUM)),
             norm1=L.BatchNormalization(k),
             norm2=L.BatchNormalization(k),
             norm3=L.BatchNormalization(k),
@@ -133,20 +133,30 @@ class MyChain(Chain):
     def __call__(self, x1, x2, test=False):
         u1_1 = self.l1_1(x1)
         u1_2 = self.l1_2(x2)
-        h1 = F.relu(self.norm1(u1_1 + u1_2, test))
-        h2 = F.relu(self.norm2(self.l2(h1), test))
-        h3 = F.relu(self.norm3(self.l3(h2), test))
-        h4 = F.relu(self.norm4(self.l4(h3), test))
-        h5 = F.relu(self.norm5(self.l5(h4), test))
-        h6 = F.relu(self.norm6(self.l6(h5), test))
-        h7 = F.relu(self.norm7(self.l7(h6), test))
-        h8 = F.relu(self.norm8(self.l8(h7), test))
-        h9 = F.relu(self.norm9(self.l9(h8), test))
-        h10 = F.relu(self.l10(h9))
-        h11 = F.relu(self.l11(h10))
-        h12 = F.relu(self.l12(h11))
-        h13 = self.l13(h12)
-        return self.l13_2(F.reshape(h13, (len(h13.data), 9*9*MOVE_DIRECTION_LABEL_NUM)))
+        u1 = u1_1 + u1_2
+        # Residual block
+        h1 = F.relu(self.norm1(u1, test))
+        h2 = F.dropout(F.relu(self.norm2(self.l2(h1), test)), ratio=dropout_ratio, train=not test)
+        u3 = self.l3(h2) + u1
+        # Residual block
+        h3 = F.relu(self.norm3(u3, test))
+        h4 = F.dropout(F.relu(self.norm4(self.l4(h3), test)), ratio=dropout_ratio, train=not test)
+        u5 = self.l5(h4) + u3
+        # Residual block
+        h5 = F.relu(self.norm5(u5, test))
+        h6 = F.dropout(F.relu(self.norm6(self.l6(h5), test)), ratio=dropout_ratio, train=not test)
+        u7 = self.l7(h6) + u5
+        # Residual block
+        h7 = F.relu(self.norm7(u7, test))
+        h8 = F.dropout(F.relu(self.norm8(self.l8(h7), test)), ratio=dropout_ratio, train=not test)
+        u9 = self.l9(h8) + u7
+        # Residual block
+        h9 = F.relu(self.norm9(u9, test))
+        h10 = F.dropout(F.relu(self.l10(h9)), ratio=dropout_ratio, train=not test)
+        u11 = self.l11(h10) + u9
+        # output
+        h12 = self.l12(u11)
+        return self.l12_2(F.reshape(h12, (len(h12.data), 9*9*MOVE_DIRECTION_LABEL_NUM)))
 
 model = MyChain()
 model.to_gpu()
