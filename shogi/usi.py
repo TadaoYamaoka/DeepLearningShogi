81e5463236493ebce1361168ef708b943b1816db
TadaoYamaoka <tadaoyamaoka@gmail.com>
2017-05-14 14:23:34 +0900

    リファクタリング

---
+++ shogi/usi.py
@@ -0,0 +1,14 @@
+import shogi
+
+def main():
+    while True:
+        line = input()
+
+        if line == 'usi':
+            print('id name test')
+            print('id author test')
+            print('usiok')
+        elif line == 'isready':
+            print('readyok')
+        elif line == 'quit':
+            break
