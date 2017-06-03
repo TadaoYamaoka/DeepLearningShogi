6147f4ecf1a7dd84d622f115ba9a62f58c9a4dd3
TadaoYamaoka <tadaoyamaoka@gmail.com>
2017-06-03 10:19:03 +0900

    Boost.pythonを使用

--- usi/usi.cpp
+++ usi/usi.cpp
@@ -1,68 +1,18 @@
-#define TMP_DEBUG _DEBUG
-#undef _DEBUG
-#include<Python.h>
-#define _DEBUG TMP_DEBUG
+#define BOOST_PYTHON_STATIC_LIB
+#define BOOST_NUMPY_STATIC_LIB
+#include <boost/python.hpp>
+
+namespace py = boost::python;
 
 int main()
 {
-	int argc = 3;
-	char* argv[] = { "", "dlshogi.usi", "main" };
-
-	PyObject *pName, *pModule, *pFunc;
-	PyObject *pArgs, *pValue;
-	int i;
-
 	Py_Initialize();
-	pName = PyUnicode_DecodeFSDefault(argv[1]);
-	/* Error checking of pName left out */
 
-	pModule = PyImport_Import(pName);
-	Py_DECREF(pName);
+	py::object dlshogi_ns = py::import("dlshogi.usi").attr("__dict__");
 
-	if (pModule != NULL) {
-		pFunc = PyObject_GetAttrString(pModule, argv[2]);
-		/* pFunc is a new reference */
+	py::object dlshogi_usi_main = dlshogi_ns["main"];
+	dlshogi_usi_main();
 
-		if (pFunc && PyCallable_Check(pFunc)) {
-			pArgs = PyTuple_New(argc - 3);
-			for (i = 0; i < argc - 3; ++i) {
-				pValue = PyLong_FromLong(atoi(argv[i + 3]));
-				if (!pValue) {
-					Py_DECREF(pArgs);
-					Py_DECREF(pModule);
-					fprintf(stderr, "Cannot convert argument\n");
-					return 1;
-				}
-				/* pValue reference stolen here: */
-				PyTuple_SetItem(pArgs, i, pValue);
-			}
-			pValue = PyObject_CallObject(pFunc, pArgs);
-			Py_DECREF(pArgs);
-			if (pValue != NULL) {
-				printf("Result of call: %ld\n", PyLong_AsLong(pValue));
-				Py_DECREF(pValue);
-			}
-			else {
-				Py_DECREF(pFunc);
-				Py_DECREF(pModule);
-				PyErr_Print();
-				fprintf(stderr, "Call failed\n");
-				return 1;
-			}
-		}
-		else {
-			if (PyErr_Occurred())
-				PyErr_Print();
-			fprintf(stderr, "Cannot find function \"%s\"\n", argv[2]);
-		}
-		Py_XDECREF(pFunc);
-		Py_DECREF(pModule);
-	}
-	else {
-		PyErr_Print();
-		fprintf(stderr, "Failed to load \"%s\"\n", argv[1]);
-		return 1;
-	}
 	Py_Finalize();
 
 	return 0;
