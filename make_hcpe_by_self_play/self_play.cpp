569da71f10e92a77fd70307d4280f0cdf8436661
TadaoYamaoka <tadaoyamaoka@gmail.com>
2018-11-21 20:04:08 +0900

    DEBUG出力オフ

--- make_hcpe_by_self_play/self_play.cpp
+++ make_hcpe_by_self_play/self_play.cpp
@@ -23,7 +23,7 @@
 #include "cppshogi.h"
 
 //#define SPDLOG_TRACE_ON
-#define SPDLOG_DEBUG_ON
+//#define SPDLOG_DEBUG_ON
 #define SPDLOG_EOL "\n"
 #include "spdlog/spdlog.h"
 auto loggersink = std::make_shared<spdlog::sinks::stdout_sink_mt>();
