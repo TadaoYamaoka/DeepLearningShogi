ecc832a9fc83e531325fc0b987ffb93947210f2f
TadaoYamaoka <tadaoyamaoka@gmail.com>
2018-12-01 14:06:56 +0900

    モデル初期化

---
+++ dlshogi/init_model.py
@@ -0,0 +1,25 @@
+﻿import numpy as np
+import chainer
+from chainer import optimizers, serializers
+
+from dlshogi.policy_value_network import *
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--model', type=str, default='model', help='model file name')
+parser.add_argument('--state', type=str, default='state', help='state file name')
+args = parser.parse_args()
+
+model = PolicyValueNetwork()
+
+optimizer = optimizers.MomentumSGD()
+optimizer.setup(model)
+
+model.cleargrads()
+optimizer.update()
+
+print('save the model')
+serializers.save_npz(args.model, model)
+print('save the optimizer')
+serializers.save_npz(args.state, optimizer)
