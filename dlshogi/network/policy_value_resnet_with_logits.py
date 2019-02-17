01c2de3b6904aa09887517f0cb557b201335592b
lvisdd <lvisdd@gmail.com>
2019-02-17 09:20:29 +0900

    Revert "BatchNormalization の axisを設定"
    This reverts commit db4d678952e6f154c1a12c50d1d741dc29f14111.

--- dlshogi/network/policy_value_resnet_with_logits.py
+++ dlshogi/network/policy_value_resnet_with_logits.py
@@ -13,11 +13,6 @@ import shogi
 
 from dlshogi.common import *
 
-L2_WEIGHT_DECAY = 1e-4
-BATCH_NORM_DECAY = 0.9
-BATCH_NORM_EPSILON = 1e-5
-BN_AXIS = 1
-
 class Bias(Layer):
 
     def __init__(self, **kwargs):
@@ -44,24 +39,12 @@ class PolicyValueResnet():
         # ショートカット元
         shortcut = input_tensor
         # メイン側
-        x = BatchNormalization(axis=BN_AXIS,
-                               momentum=BATCH_NORM_DECAY,
-                               epsilon=BATCH_NORM_EPSILON)(input_tensor)
+        x = BatchNormalization()(input_tensor)
         x = Activation('relu')(x)
-        x = Conv2D(channles, (3, 3), use_bias=False,
-                   padding='same',
-                   kernel_initializer='he_normal',
-                   kernel_regularizer=regularizers.l2(L2_WEIGHT_DECAY),
-                   data_format='channels_first')(x)
-        x = BatchNormalization(axis=BN_AXIS,
-                               momentum=BATCH_NORM_DECAY,
-                               epsilon=BATCH_NORM_EPSILON)(x)
+        x = Conv2D(channles, (3, 3), padding='same', data_format='channels_first')(x)
+        x = BatchNormalization()(x)
         x = Activation('relu')(x)
-        x = Conv2D(channles, (3, 3), use_bias=False,
-                   padding='same',
-                   kernel_initializer='he_normal',
-                   kernel_regularizer=regularizers.l2(L2_WEIGHT_DECAY),
-                   data_format='channels_first')(x)
+        x = Conv2D(channles, (3, 3), padding='same', data_format='channels_first')(x)
         # 結合
         return Add()([x, shortcut])
     
@@ -70,11 +53,7 @@ class PolicyValueResnet():
         main_input = Input(shape=(104, 9, 9))
         
         # layer1
-        x = Conv2D(k, (3, 3), use_bias=False,
-                   padding='same',
-                   kernel_initializer='he_normal',
-                   kernel_regularizer=regularizers.l2(L2_WEIGHT_DECAY),
-                   data_format='channels_first')(main_input)
+        x = Conv2D(k, (3, 3), padding='same', data_format='channels_first')(main_input)
         
         # layer2 - 12
         for i in range(11):
@@ -82,19 +61,13 @@ class PolicyValueResnet():
         
         # policy network
         # layer13
-        ph = Conv2D(MOVE_DIRECTION_LABEL_NUM, (1, 1), use_bias=False,
-                    kernel_initializer='he_normal',
-                    kernel_regularizer=regularizers.l2(L2_WEIGHT_DECAY),
-                    data_format='channels_first')(x)
+        ph = Conv2D(MOVE_DIRECTION_LABEL_NUM, (1, 1), data_format='channels_first', use_bias=False)(x)
         ph = Flatten()(ph)
         ph = Bias(name = 'policy_head')(ph)
 
         # value network
         # layer13
-        vh = Conv2D(MOVE_DIRECTION_LABEL_NUM, (1, 1), use_bias=False,
-                    kernel_initializer='he_normal',
-                    kernel_regularizer=regularizers.l2(L2_WEIGHT_DECAY),
-                    data_format='channels_first')(x)
+        vh = Conv2D(MOVE_DIRECTION_LABEL_NUM, (1, 1), data_format='channels_first', use_bias=False)(x)
         vh = Flatten()(vh)
         vh = Dense(units=256, activation='relu', input_dim=NUM_CLASSES)(vh)
         vh = Dense(units=1, activation="tanh", input_dim=256, name = 'value_head')(vh)
