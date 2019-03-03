ef9b4fc5e2e26a6fd93c8319f0c5753690b734b2
lvisdd <lvisdd@gmail.com>
2019-03-04 08:17:48 +0900

    hcpe形式に対応

--- dlshogi/read_kifu.py
+++ dlshogi/read_kifu.py
@@ -4,6 +4,9 @@ import copy
 
 from dlshogi.features import *
 
+import numpy as np
+from cshogi import *
+
 # read kifu
 def read_kifu(kifu_list_file):
     positions = []
@@ -31,4 +34,49 @@ def read_kifu(kifu_list_file):
 
                 positions.append((piece_bb, occupied, pieces_in_hand, move_label, win))
                 board.push_usi(move)
+    return positions
+
+def read_kifu_from_hcpe(hcpe_path):
+    hcpes = np.fromfile(hcpe_path, dtype=HuffmanCodedPosAndEval)
+    positions = []
+    for i, hcpe in enumerate(hcpes):
+        board = Board()        
+        board.set_hcp(hcpes[i]['hcp'])
+        
+        sfen = board.sfen()
+        pboard = shogi.Board(sfen=sfen.decode('utf-8'))
+        if pboard.turn == shogi.BLACK:
+            piece_bb = copy.deepcopy(pboard.piece_bb)
+            occupied = copy.deepcopy((pboard.occupied[shogi.BLACK], pboard.occupied[shogi.WHITE]))
+            pieces_in_hand = copy.deepcopy((pboard.pieces_in_hand[shogi.BLACK], pboard.pieces_in_hand[shogi.WHITE]))
+        else:
+            piece_bb = [bb_rotate_180(bb) for bb in pboard.piece_bb]
+            occupied = (bb_rotate_180(pboard.occupied[shogi.WHITE]), bb_rotate_180(pboard.occupied[shogi.BLACK]))
+            pieces_in_hand = copy.deepcopy((pboard.pieces_in_hand[shogi.WHITE], pboard.pieces_in_hand[shogi.BLACK]))
+        
+        # move label
+        move = hcpes[i]['bestMove16']
+        move_label = make_output_label(shogi.Move.from_usi(move_to_usi(move).decode('utf-8')), pboard.turn)
+        
+        # result
+        gameResult = hcpes[i]['gameResult']
+        if board.turn == BLACK:
+            if gameResult == BLACK_WIN:
+                win_color = 1
+            if gameResult == WHITE_WIN:
+                win_color = -1
+            else:
+                win_color = 0
+        else:
+            if gameResult == BLACK_WIN:
+                win_color = -1
+            if gameResult == WHITE_WIN:
+                win_color = 1
+            else:
+                win_color = 0
+
+        win = 1 if win_color == pboard.turn else 0
+        
+        positions.append((piece_bb, occupied, pieces_in_hand, move_label, win))
+        
     return positions
