b7308e3c09ad71411653df87b5920c3a8ff9d357
lvisdd <lvisdd@gmail.com>
2019-03-16 22:57:32 +0900

    TPUで学習率減衰させる

--- train_policy_value_resnet_with_logits_experiment.py
+++ train_policy_value_resnet_with_logits_experiment.py
@@ -99,6 +99,14 @@ if args.use_tpu:
 else:
     optimizer = SGD(lr=0.001, momentum=0.9)
 
+model.compile(loss={'policy_head': categorical_crossentropy, 'value_head': 'mean_squared_error'},
+              optimizer=optimizer,
+              loss_weights={'policy_head': 0.5, 'value_head': 0.5},
+              metrics=['accuracy', categorical_accuracy])
+
+checkpoint_path = args.model + "/model_policy_value_resnet_with_logits-best.hdf5"
+checkpoint = ModelCheckpoint(checkpoint_path, verbose=1, save_best_only=True)
+
 if args.use_tpu:
     # TPU
     import tensorflow as tf
