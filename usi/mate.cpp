4945886163d3c5fe43a28914c8f6bdb2022d0c84
TadaoYamaoka <tadaoyamaoka@gmail.com>
2019-12-29 18:52:25 +0900

    詰み探索で王手している角の利きには移動してしまうバグ

--- usi/mate.cpp
+++ usi/mate.cpp
@@ -16,7 +16,6 @@ public:
 			if (INCHECK) {
 				// 自玉が王手の場合、逃げる手かつ王手をかける手を生成
 				ExtMove* curr = moveList_;
-				const Bitboard pinned = pos.pinnedBB();
 				while (curr != last_) {
 					if (!pos.moveIsPseudoLegal<false>(curr->move))
 						curr->move = (--last_)->move;
