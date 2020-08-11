d12a7e73ea71e5b0513361d2592dbbf7b2719689
48 <bleu48@gmail.com>
2020-08-11 15:49:52 +0900

    Pytorch 1.6 Automatic Mixed Precision対応
    Pytorch 1.6で新規に導入されたAutomatic Mixed Precisionに対応することによりTensorCore搭載GPUで省メモリ高速化が可能になる。演算精度低下の影響は調査中。

---
+++ dlshogi/train_rl_policy_with_value_using_hcpe_bootstrap_amp.py
@@ -0,0 +1,276 @@
+﻿import numpy as np
+import torch
+import torch.optim as optim
+
+from dlshogi.common import *
+from dlshogi import serializers
+from dlshogi.swa import SWA
+
+from dlshogi import cppshogi
+
+import argparse
+import random
+import os
+
+import logging
+
+parser = argparse.ArgumentParser(description='Traning RL policy network using hcpe')
+parser.add_argument('train_data', type=str, nargs='+', help='train data file')
+parser.add_argument('test_data', type=str, help='test data file')
+parser.add_argument('--batchsize', '-b', type=int, default=1024, help='Number of positions in each mini-batch')
+parser.add_argument('--testbatchsize', type=int, default=640, help='Number of positions in each test mini-batch')
+parser.add_argument('--epoch', '-e', type=int, default=1, help='Number of epoch times')
+parser.add_argument('--network', type=str, default='wideresnet10', choices=['wideresnet10', 'wideresnet15', 'senet10'], help='network type')
+parser.add_argument('--model', type=str, default='model_rl_val_hcpe', help='model file name')
+parser.add_argument('--state', type=str, default='state_rl_val_hcpe', help='state file name')
+parser.add_argument('--initmodel', '-m', default='', help='Initialize the model from given file')
+parser.add_argument('--resume', '-r', default='', help='Resume the optimization from snapshot')
+parser.add_argument('--log', default=None, help='log file path')
+parser.add_argument('--lr', type=float, default=0.01, help='learning rate')
+parser.add_argument('--weightdecay_rate', type=float, default=0.0001, help='weightdecay rate')
+parser.add_argument('--beta', type=float, default=0.001, help='entropy regularization coeff')
+parser.add_argument('--val_lambda', type=float, default=0.333, help='regularization factor')
+parser.add_argument('--gpu', '-g', type=int, default=0, help='GPU ID')
+parser.add_argument('--eval_interval', type=int, default=1000, help='evaluation interval')
+parser.add_argument('--swa_freq', type=int, default=250)
+parser.add_argument('--swa_n_avr', type=int, default=10)
+args = parser.parse_args()
+
+if args.network == 'wideresnet15':
+    from dlshogi.policy_value_network_wideresnet15 import *
+elif args.network == 'senet10':
+    from dlshogi.policy_value_network_senet10 import *
+else:
+    from dlshogi.policy_value_network import *
+
+logging.basicConfig(format='%(asctime)s\t%(levelname)s\t%(message)s', datefmt='%Y/%m/%d %H:%M:%S', filename=args.log, level=logging.DEBUG)
+logging.info('batchsize={}'.format(args.batchsize))
+logging.info('MomentumSGD(lr={})'.format(args.lr))
+logging.info('WeightDecay(rate={})'.format(args.weightdecay_rate))
+logging.info('entropy regularization coeff={}'.format(args.beta))
+logging.info('val_lambda={}'.format(args.val_lambda))
+
+if args.gpu >= 0:
+    torch.cuda.set_device(args.gpu)
+    device = torch.device("cuda")
+else:
+    device = torch.device("cpu")
+
+model = PolicyValueNetwork()
+model.to(device)
+
+base_optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=0.9, weight_decay=args.weightdecay_rate, nesterov=True)
+optimizer = SWA(base_optimizer, swa_start=args.swa_freq, swa_freq=args.swa_freq, swa_lr=0.05, swa_n_avr=args.swa_n_avr)
+cross_entropy_loss = torch.nn.CrossEntropyLoss(reduction='none')
+bce_with_logits_loss = torch.nn.BCEWithLogitsLoss()
+scaler = torch.cuda.amp.GradScaler()
+
+# Init/Resume
+if args.initmodel:
+    print('Load model from', args.initmodel)
+    serializers.load_npz(args.initmodel, model)
+if args.resume:
+    print('Load optimizer state from', args.resume)
+    checkpoint = torch.load(args.resume)
+    epoch = checkpoint['epoch']
+    t = checkpoint['t']
+    base_optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+else:
+    epoch = 0
+    t = 0
+
+logging.debug('read teacher data')
+def load_teacher(files):
+    data = []
+    for path in files:
+        if os.path.exists(path):
+            logging.debug(path)
+            data.append(np.fromfile(path, dtype=HuffmanCodedPosAndEval))
+        else:
+            logging.debug('{} not found, skipping'.format(path))
+    return np.concatenate(data)
+train_data = load_teacher(args.train_data)
+logging.debug('read test data')
+logging.debug(args.test_data)
+test_data = np.fromfile(args.test_data, dtype=HuffmanCodedPosAndEval)
+
+logging.info('train position num = {}'.format(len(train_data)))
+logging.info('test position num = {}'.format(len(test_data)))
+
+# mini batch
+def mini_batch(hcpevec):
+    features1 = np.empty((len(hcpevec), FEATURES1_NUM, 9, 9), dtype=np.float32)
+    features2 = np.empty((len(hcpevec), FEATURES2_NUM, 9, 9), dtype=np.float32)
+    move = np.empty((len(hcpevec)), dtype=np.int32)
+    result = np.empty((len(hcpevec)), dtype=np.float32)
+    value = np.empty((len(hcpevec)), dtype=np.float32)
+
+    cppshogi.hcpe_decode_with_value(hcpevec, features1, features2, move, result, value)
+
+    z = result.astype(np.float32) - value + 0.5
+
+    return (torch.tensor(features1).to(device),
+            torch.tensor(features2).to(device),
+            torch.tensor(move.astype(np.int64)).to(device),
+            torch.tensor(result.reshape((len(hcpevec), 1))).to(device),
+            torch.tensor(z).to(device),
+            torch.tensor(value.reshape((len(value), 1))).to(device)
+            )
+
+# for SWA bn_update
+def hcpe_loader(data, batchsize):
+    for i in range(0, len(data) - batchsize + 1, batchsize):
+        x1, x2, t1, t2, z, value = mini_batch(data[i:i+batchsize])
+        yield x1, x2
+
+def accuracy(y, t):
+    return (torch.max(y, 1)[1] == t).sum().item() / len(t)
+
+def binary_accuracy(y, t):
+    pred = y >= 0
+    truth = t >= 0.5
+    return pred.eq(truth).sum().item() / len(t)
+
+# train
+itr = 0
+sum_loss1 = 0
+sum_loss2 = 0
+sum_loss3 = 0
+sum_loss = 0
+eval_interval = args.eval_interval
+for e in range(args.epoch):
+    np.random.shuffle(train_data)
+
+    itr_epoch = 0
+    sum_loss1_epoch = 0
+    sum_loss2_epoch = 0
+    sum_loss3_epoch = 0
+    sum_loss_epoch = 0
+    for i in range(0, len(train_data) - args.batchsize + 1, args.batchsize):
+        with torch.cuda.amp.autocast(): 
+            model.train()
+
+            x1, x2, t1, t2, z, value = mini_batch(train_data[i:i+args.batchsize])
+            y1, y2 = model(x1, x2)
+
+            model.zero_grad()
+            loss1 = (cross_entropy_loss(y1, t1) * z).mean()
+            if args.beta > 0:
+                loss1 += args.beta * (F.softmax(y1, dim=1) * F.log_softmax(y1, dim=1)).sum(dim=1).mean()
+            loss2 = bce_with_logits_loss(y2, t2)
+            loss3 = bce_with_logits_loss(y2, value)
+            loss = loss1 + (1 - args.val_lambda) * loss2 + args.val_lambda * loss3
+        # loss.backward()
+        scaler.scale(loss).backward()
+        # optimizer.step()
+        scaler.step(optimizer)
+        scaler.update()
+
+        t += 1
+        itr += 1
+        sum_loss1 += loss1.item()
+        sum_loss2 += loss2.item()
+        sum_loss3 += loss3.item()
+        sum_loss += loss.item()
+        itr_epoch += 1
+        sum_loss1_epoch += loss1.item()
+        sum_loss2_epoch += loss2.item()
+        sum_loss3_epoch += loss3.item()
+        sum_loss_epoch += loss.item()
+
+        # print train loss
+        if t % eval_interval == 0:
+            model.eval()
+
+            x1, x2, t1, t2, z, value = mini_batch(np.random.choice(test_data, args.testbatchsize))
+            with torch.no_grad():
+                y1, y2 = model(x1, x2)
+
+                loss1 = (cross_entropy_loss(y1, t1) * z).mean()
+                loss2 = bce_with_logits_loss(y2, t2)
+                loss3 = bce_with_logits_loss(y2, value)
+                loss = loss1 + (1 - args.val_lambda) * loss2 + args.val_lambda * loss3
+
+                logging.info('epoch = {}, iteration = {}, loss = {:.08f}, {:.08f}, {:.08f}, {:.08f}, test loss = {:.08f}, {:.08f}, {:.08f}, {:.08f}, test accuracy = {:.08f}, {:.08f}'.format(
+                    epoch + 1, t,
+                    sum_loss1 / itr, sum_loss2 / itr, sum_loss3 / itr, sum_loss / itr,
+                    loss1.item(), loss2.item(), loss3.item(), loss.item(),
+                    accuracy(y1, t1), binary_accuracy(y2, t2)))
+            itr = 0
+            sum_loss1 = 0
+            sum_loss2 = 0
+            sum_loss3 = 0
+            sum_loss = 0
+
+# Interval Output
+        if t % (eval_interval*5) == 0:
+            print('save the model')
+            serializers.save_npz(args.model+"_tmp", model)
+            print('save the optimizer')
+            torch.save({
+                'epoch': epoch,
+                't': t,
+                'optimizer_state_dict': base_optimizer.state_dict(),
+                }, args.state+"_tmp")
+
+
+    optimizer.swap_swa_sgd()
+    optimizer.bn_update(hcpe_loader(train_data, args.batchsize), model)
+
+    # print train loss for each epoch
+    itr_test = 0
+    sum_test_loss1 = 0
+    sum_test_loss2 = 0
+    sum_test_loss3 = 0
+    sum_test_loss = 0
+    sum_test_accuracy1 = 0
+    sum_test_accuracy2 = 0
+    sum_test_entropy1 = 0
+    sum_test_entropy2 = 0
+    model.eval()
+    with torch.no_grad():
+        for i in range(0, len(test_data) - args.testbatchsize, args.testbatchsize):
+            x1, x2, t1, t2, z, value = mini_batch(test_data[i:i+args.testbatchsize])
+            y1, y2 = model(x1, x2)
+
+            itr_test += 1
+            loss1 = (cross_entropy_loss(y1, t1) * z).mean()
+            loss2 = bce_with_logits_loss(y2, t2)
+            loss3 = bce_with_logits_loss(y2, value)
+            loss = loss1 + (1 - args.val_lambda) * loss2 + args.val_lambda * loss3
+            sum_test_loss1 += loss1.item()
+            sum_test_loss2 += loss2.item()
+            sum_test_loss3 += loss3.item()
+            sum_test_loss += loss.item()
+            sum_test_accuracy1 += accuracy(y1, t1)
+            sum_test_accuracy2 += binary_accuracy(y2, t2)
+
+            entropy1 = (- F.softmax(y1, dim=1) * F.log_softmax(y1, dim=1)).sum(dim=1)
+            sum_test_entropy1 += entropy1.mean().item()
+
+            p2 = y2.sigmoid()
+            #entropy2 = -(p2 * F.log(p2) + (1 - p2) * F.log(1 - p2))
+            log1p_ey2 = F.softplus(y2)
+            entropy2 = -(p2 * (y2 - log1p_ey2) + (1 - p2) * -log1p_ey2)
+            sum_test_entropy2 +=entropy2.mean().item()
+
+        logging.info('epoch = {}, iteration = {}, train loss avr = {:.08f}, {:.08f}, {:.08f}, {:.08f}, test_loss = {:.08f}, {:.08f}, {:.08f}, {:.08f}, test accuracy = {:.08f}, {:.08f}, test entropy = {:.08f}, {:.08f}'.format(
+            epoch + 1, t,
+            sum_loss1_epoch / itr_epoch, sum_loss2_epoch / itr_epoch, sum_loss3_epoch / itr_epoch, sum_loss_epoch / itr_epoch,
+            sum_test_loss1 / itr_test, sum_test_loss2 / itr_test, sum_test_loss3 / itr_test, sum_test_loss / itr_test,
+            sum_test_accuracy1 / itr_test, sum_test_accuracy2 / itr_test,
+            sum_test_entropy1 / itr_test, sum_test_entropy2 / itr_test))
+
+    epoch += 1
+
+    if e != args.epoch - 1:
+        optimizer.swap_swa_sgd()
+
+print('save the model')
+serializers.save_npz(args.model, model)
+print('save the optimizer')
+torch.save({
+    'epoch': epoch,
+    't': t,
+    'optimizer_state_dict': base_optimizer.state_dict(),
+    }, args.state)
