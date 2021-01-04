b878c9584d8b61754ec6a2dbac6c2c49a2bb39fb
TadaoYamaoka <tadaoyamaoka@gmail.com>
2021-01-04 19:23:51 +0900

    MultiPV実装

--- usi/main.cpp
+++ usi/main.cpp
@@ -162,6 +162,7 @@ void MySearcher::doUSICommandLoop(int argc, char* argv[]) {
 			c_fpu_reduction_root = options["C_fpu_reduction_root"] / 100.0f;
 			SetReuseSubtree(options["ReuseSubtree"]);
 			SetPvInterval(options["PV_Interval"]);
+			SetMultiPV(options["MultiPV"]);
 
 			// DebugMessageMode
 			SetDebugMessageMode(options["DebugMessage"]);
