f286a8b63efe0efce61a69daeb877ae1876183a1
TadaoYamaoka <tadaoyamaoka@gmail.com>
2021-02-12 21:50:01 +0900

    fixup

---
+++ dlshogi/policy_value_network_resnet_nofixup.py
@@ -0,0 +1,61 @@
+﻿import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from dlshogi.common import *
+
+class FixupBlock(nn.Module):
+    def __init__(self, filters):
+        super(FixupBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=filters, out_channels=filters, kernel_size=3, padding=1, bias=False)
+        self.conv2 = nn.Conv2d(in_channels=filters, out_channels=filters, kernel_size=3, padding=1, bias=False)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = torch.relu_(out)
+
+        out = self.conv2(out)
+
+        out += x
+        out = torch.relu_(out)
+
+        return out
+
+class PolicyValueNetwork(nn.Module):
+    def __init__(self, blocks=10, filters=192, units=256):
+        super(PolicyValueNetwork, self).__init__()
+        self.conv1_1_1 = nn.Conv2d(in_channels=FEATURES1_NUM, out_channels=filters, kernel_size=3, padding=1, bias=False)
+        self.conv1_1_2 = nn.Conv2d(in_channels=FEATURES1_NUM, out_channels=filters, kernel_size=1, padding=0, bias=False)
+        self.conv1_2 = nn.Conv2d(in_channels=FEATURES2_NUM, out_channels=filters, kernel_size=1, bias=False) # pieces_in_hand
+
+        # residual blocks
+        self.blocks = nn.Sequential(*[FixupBlock(filters) for _ in range(blocks)])
+
+        # policy network
+        self.policy_conv1 = nn.Conv2d(in_channels=filters, out_channels=MAX_MOVE_LABEL_NUM, kernel_size=1, bias=False)
+        self.policy_bias1 = nn.Parameter(torch.zeros(9*9*MAX_MOVE_LABEL_NUM))
+
+        # value network
+        self.value_conv1 = nn.Conv2d(in_channels=filters, out_channels=MAX_MOVE_LABEL_NUM, kernel_size=1)
+        self.value_fc1 = nn.Linear(9*9*MAX_MOVE_LABEL_NUM, units)
+        self.value_fc2 = nn.Linear(units, 1)
+
+    def __call__(self, x1, x2):
+        u1_1_1 = self.conv1_1_1(x1)
+        u1_1_2 = self.conv1_1_2(x1)
+        u1_2 = self.conv1_2(x2)
+        x = torch.relu_(u1_1_1 + u1_1_2 + u1_2)
+
+        # residual blocks
+        x = self.blocks(x)
+
+        # policy network
+        h_p = self.policy_conv1(x)
+        policy = h_p.view(-1, 9*9*MAX_MOVE_LABEL_NUM) + self.policy_bias1
+
+        # value network
+        h_v = torch.relu_(self.value_conv1(x))
+        h_v = torch.relu_(self.value_fc1(h_v.view(-1, 9*9*MAX_MOVE_LABEL_NUM)))
+        value = self.value_fc2(h_v)
+
+        return policy, value
--- dlshogi/train_rl_policy_with_value_using_hcpe_bootstrap.py
+++ dlshogi/train_rl_policy_with_value_using_hcpe_bootstrap.py
@@ -20,7 +20,7 @@ parser.add_argument('test_data', type=str, help='test data file')
 parser.add_argument('--batchsize', '-b', type=int, default=1024, help='Number of positions in each mini-batch')
 parser.add_argument('--testbatchsize', type=int, default=640, help='Number of positions in each test mini-batch')
 parser.add_argument('--epoch', '-e', type=int, default=1, help='Number of epoch times')
-parser.add_argument('--network', type=str, default='wideresnet10', choices=['wideresnet10', 'wideresnet15', 'senet10', 'resnet10_swish', 'resnet20_swish'], help='network type')
+parser.add_argument('--network', type=str, default='wideresnet10', choices=['wideresnet10', 'wideresnet15', 'senet10', 'resnet10_swish', 'resnet20_swish', 'resnet10_fixup', 'resnet10_fixup2', 'resnet10_nofixup'], help='network type')
 parser.add_argument('--model', type=str, default='model_rl_val_hcpe', help='model file name')
 parser.add_argument('--state', type=str, default='state_rl_val_hcpe', help='state file name')
 parser.add_argument('--initmodel', '-m', default='', help='Initialize the model from given file')
@@ -41,14 +41,28 @@ args = parser.parse_args()
 
 if args.network == 'wideresnet15':
     from dlshogi.policy_value_network_wideresnet15 import *
+    model = PolicyValueNetwork()
 elif args.network == 'senet10':
     from dlshogi.policy_value_network_senet10 import *
+    model = PolicyValueNetwork()
 elif args.network == 'resnet10_swish':
     from dlshogi.policy_value_network_resnet10_swish import *
+    model = PolicyValueNetwork()
 elif args.network == 'resnet20_swish':
     from dlshogi.policy_value_network_resnet20_swish import *
+    model = PolicyValueNetwork()
+elif args.network == 'resnet10_fixup':
+    from dlshogi.policy_value_network_resnet_fixup import *
+    model = PolicyValueNetwork(blocks=10, filters=192, units=256)
+elif args.network == 'resnet10_fixup2':
+    from dlshogi.policy_value_network_resnet_fixup2 import *
+    model = PolicyValueNetwork(blocks=10, filters=192, units=256)
+elif args.network == 'resnet10_nofixup':
+    from dlshogi.policy_value_network_resnet_nofixup import *
+    model = PolicyValueNetwork(blocks=10, filters=192, units=256)
 else:
     from dlshogi.policy_value_network import *
+    model = PolicyValueNetwork()
 
 logging.basicConfig(format='%(asctime)s\t%(levelname)s\t%(message)s', datefmt='%Y/%m/%d %H:%M:%S', filename=args.log, level=logging.DEBUG)
 logging.info('batchsize={}'.format(args.batchsize))
@@ -63,7 +77,6 @@ if args.gpu >= 0:
 else:
     device = torch.device("cpu")
 
-model = PolicyValueNetwork()
 model.to(device)
 
 base_optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=0.9, weight_decay=args.weightdecay_rate, nesterov=True)
@@ -231,7 +244,8 @@ for e in range(args.epoch):
         amp_context = torch.cuda.amp.autocast()
         amp_context.__enter__()
 
-    optimizer.bn_update(hcpe_loader(train_data, args.batchsize), model)
+    if 'fixup' not in args.network:
+        optimizer.bn_update(hcpe_loader(train_data, args.batchsize), model)
 
     if args.use_amp:
         amp_context.__exit__()
