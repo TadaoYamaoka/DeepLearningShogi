1af8ce1707cc97a78a088d048eb8b2dbce58c20c
TadaoYamaoka <tadaoyamaoka@gmail.com>
2021-05-23 16:49:18 +0900

    Fix typo.

--- dlshogi/train_hcpe.py
+++ dlshogi/train_hcpe.py
@@ -229,7 +229,7 @@ for e in range(args.epoch):
             entropy2 = -(p2 * (y2 - log1p_ey2) + (1 - p2) * -log1p_ey2)
             sum_test_entropy2 +=entropy2.mean().item()
 
-        logging.info('epoch = {}, iteration = {}, train loss avr = {:.08f}, {:.08f}, {:.08f}, {:.08f}, test_loss = {:.08f}, {:.08f}, {:.08f}, {:.08f}, test accuracy = {:.08f}, {:.08f}, test entropy = {:.08f}, {:.08f}'.format(
+        logging.info('epoch = {}, iteration = {}, train loss avr = {:.08f}, {:.08f}, {:.08f}, {:.08f}, test loss = {:.08f}, {:.08f}, {:.08f}, {:.08f}, test accuracy = {:.08f}, {:.08f}, test entropy = {:.08f}, {:.08f}'.format(
             epoch + 1, t,
             sum_loss1_epoch / itr_epoch, sum_loss2_epoch / itr_epoch, sum_loss3_epoch / itr_epoch, sum_loss_epoch / itr_epoch,
             sum_test_loss1 / itr_test, sum_test_loss2 / itr_test, sum_test_loss3 / itr_test, sum_test_loss / itr_test,
