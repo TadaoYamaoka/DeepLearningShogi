ad4197c175fa0a1248af05f90c342759db6b1564
TadaoYamaoka <tadaoyamaoka@gmail.com>
2021-06-20 14:12:02 +0900

    レーティングでフィルタ

--- dlshogi/utils/stat_csa_in_dir.py
+++ dlshogi/utils/stat_csa_in_dir.py
@@ -1,31 +1,34 @@
 ﻿import argparse
 import os
-import sys
+import glob
 from cshogi import *
 from cshogi import CSA
-import statistics
+import pandas as pd
 import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser()
 parser.add_argument('dir', type=str)
+parser.add_argument('--rating', type=int, default=3800)
 parser.add_argument('--plot', action='store_true')
 args = parser.parse_args()
 
-def find_all_files(directory):
-    for root, dirs, files in os.walk(directory):
-        for file in files:
-            yield os.path.join(root, file)
-
-kifu_num = 0
 kifu_moves_len = []
+sennichite_num = 0
 kachi_num = 0
 #max_hand_fu = []
-for filepath in find_all_files(args.dir):
+for filepath in glob.glob(os.path.join(args.dir, '**', '*.csa'), recursive=True):
     for kifu in CSA.Parser.parse_file(filepath):
-        kifu_num += 1
+        if any([r < args.rating for r in kifu.ratings]):
+            continue
+
+        if kifu.endgame not in ('%TORYO', '%SENNICHITE', '%JISHOGI', '%KACHI'):
+            continue
+
         moves_len = len(kifu.moves)
         kifu_moves_len.append(moves_len)
-        if kifu.endgame == '%KACHI':
+        if kifu.endgame == '%SENNICHITE':
+            sennichite_num += 1
+        elif kifu.endgame == '%KACHI':
             kachi_num += 1
         #board = Board()
         #max_hand_fu_in_game = 0
@@ -34,17 +37,14 @@ for filepath in find_all_files(args.dir):
         #    max_hand_fu_in_game = max(max_hand_fu_in_game, board.pieces_in_hand[BLACK][HPAWN], board.pieces_in_hand[WHITE][HPAWN])
         #max_hand_fu.append(max_hand_fu_in_game)
 
-print('kifu num : {}'.format(kifu_num))
-print('moves sum : {}'.format(sum(kifu_moves_len)))
-print('moves mean : {}'.format(statistics.mean(kifu_moves_len)))
-print('moves median : {}'.format(statistics.median(kifu_moves_len)))
-print('moves max : {}'.format(max(kifu_moves_len)))
-print('moves min : {}'.format(min(kifu_moves_len)))
-print('nyugyoku kachi : {}'.format(kachi_num))
+df = pd.DataFrame(kifu_moves_len, columns=['moves'])
+print(df.describe())
+print('sennichite : {}'.format(sennichite_num))
+print('nyugyoku : {}'.format(kachi_num))
 #print('max hand fu : {}'.format(max(max_hand_fu)))
 
 if args.plot:
-    plt.hist(kifu_moves_len)
+    df.hist()
     plt.show()
 
     #plt.hist(max_hand_fu, bins=18, range=(0, 18))
