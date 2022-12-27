9f8134d13b01ed3d3392df43d8e362d3888cd217
TadaoYamaoka <tadaoyamaoka@gmail.com>
2022-12-27 22:48:57 +0900

    負けた方の評価値が正しくない局面をsfen出力するオプション(--lose_sfen)追加

--- dlshogi/utils/csa_important_position.py
+++ dlshogi/utils/csa_important_position.py
@@ -7,11 +7,13 @@ import glob
 
 parser = argparse.ArgumentParser()
 parser.add_argument('csa_dir')
-parser.add_argument('--diff', type=int, default=300)
+parser.add_argument('--diff', type=int, default=500)
 parser.add_argument('--win_name')
+parser.add_argument('--lose_sfen')
 args = parser.parse_args()
 
 board = Board()
+lose_sfen = []
 for filepath in glob.glob(os.path.join(args.csa_dir, '**', '*.csa'), recursive=True):
     for kif in CSA.Parser.parse_file(filepath):
         if kif.endgame not in ('%TORYO', '%KACHI'):
@@ -32,16 +34,30 @@ for filepath in glob.glob(os.path.join(args.csa_dir, '**', '*.csa'), recursive=T
             if score * prev_score < 0 and abs(score - prev_score) > args.diff:
                 if (score > 0 and kif.win - 1 == board.turn) or (score < 0 and kif.win - 1 != board.turn):
                     if i % 2 != kif.win - 1:
-                        i += 1
-                    base_score = int(kif.scores[i] * 0.9)
+                        j = i + 1
+                    else:
+                        j = i
+                    base_score = int(kif.scores[j] * 0.9)
                     ok = True
-                    for after_score in kif.scores[i::2]:
+                    for after_score in kif.scores[j::2]:
                         if (base_score > 0 and after_score < base_score) or (base_score < 0 and after_score > base_score):
                             ok = False
                             break
                     if ok:
                         print(filepath, board.sfen(), sep='\t')
+                        if args.lose_sfen:
+                            for k, (move, score) in enumerate(zip(kif.moves[i:], kif.scores[i:])):
+                                # •‰‚¯‚½•û‚ÌŽè”Ô
+                                if (i + k) % 2 != j % 2:
+                                    if (base_score > 0 and score > 100) or (base_score < 0 and score < -100):
+                                        break
+                                    lose_sfen.append('startpos moves ' + ' '.join([move_to_usi(move) for move in board.history]) + '\n')
+                                board.push(move)
                         break
 
             board.push(move)
             prev_score = score
+
+if args.lose_sfen:
+    with open(args.lose_sfen, 'w', newline='\n') as f:
+        f.writelines(lose_sfen)
