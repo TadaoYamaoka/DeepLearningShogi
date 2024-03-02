f7169a1143629128799a4c5a5ab501576488d30a
TadaoYamaoka <tadaoyamaoka@gmail.com>
2024-03-02 09:58:34 +0900

    EMA対応

--- dlshogi/ptl.py
+++ dlshogi/ptl.py
@@ -4,7 +4,9 @@ import lightning.pytorch as pl
 import numpy as np
 import torch
 import torch.nn.functional as F
+from lightning.pytorch.callbacks.progress.tqdm_progress import Tqdm, TQDMProgressBar
 from lightning.pytorch.cli import LightningCLI
+from torch.optim.swa_utils import AveragedModel, get_ema_multi_avg_fn, update_bn
 from torch.utils.data import DataLoader, Dataset
 
 from dlshogi import cppshogi
@@ -135,7 +137,7 @@ class DataModule(pl.LightningDataModule):
 
         # Assign test dataset for use in dataloader(s)
         if stage == "test" or stage == "predict":
-            self.test_dataset = HcpeDataset(self.hparams.val_files)
+            self.val_dataset = HcpeDataset(self.hparams.val_files)
 
     def train_dataloader(self):
         return DataLoader(
@@ -147,16 +149,18 @@ class DataModule(pl.LightningDataModule):
 
     def val_dataloader(self):
         return DataLoader(
-            self.val_dataset,
-            batch_size=self.hparams.val_batch_size,
-            collate_fn=collate,
+            self.val_dataset, batch_size=self.hparams.val_batch_size, collate_fn=collate
         )
 
     def test_dataloader(self):
-        return DataLoader(self.val_dataset, batch_size=self.hparams.val_batch_size)
+        return DataLoader(
+            self.val_dataset, batch_size=self.hparams.val_batch_size, collate_fn=collate
+        )
 
     def predict_dataloader(self):
-        return DataLoader(self.val_dataset, batch_size=self.hparams.val_batch_size)
+        return DataLoader(
+            self.val_dataset, batch_size=self.hparams.val_batch_size, collate_fn=collate
+        )
 
 
 def cross_entropy_loss_with_soft_target(pred, soft_targets):
@@ -178,10 +182,23 @@ def binary_accuracy(y, t):
 
 
 class Model(pl.LightningModule):
-    def __init__(self, network="resnet10_relu", val_lambda=0.333):
+    def __init__(
+        self,
+        network="resnet10_relu",
+        val_lambda=0.333,
+        use_ema=False,
+        update_bn=True,
+        ema_start_epoch=1,
+        ema_freq=250,
+        ema_decay=0.9,
+    ):
         super().__init__()
         self.save_hyperparameters()
         self.model = policy_value_network(network)
+        if use_ema:
+            self.ema_model = AveragedModel(
+                self.model, multi_avg_fn=get_ema_multi_avg_fn(ema_decay)
+            )
         self.validation_step_outputs = defaultdict(list)
 
     def training_step(self, batch, batch_idx):
@@ -197,6 +214,37 @@ class Model(pl.LightningModule):
         )
         self.log("train/loss", loss)
         return loss
+
+    def on_train_batch_end(self, outputs, batch, batch_idx):
+        if (
+            self.hparams.use_ema
+            and self.current_epoch >= self.hparams.ema_start_epoch
+            and self.global_step % self.hparams.ema_freq == 0
+        ):
+            self.ema_model.update_parameters(self.model)
+
+    def on_train_epoch_end(self):
+        if (
+            self.hparams.use_ema
+            and self.hparams.update_bn
+            and self.current_epoch == self.trainer.max_epochs - 1
+            and self.current_epoch >= self.hparams.ema_start_epoch
+        ):
+
+            def data_loader():
+                for x1, x2, _, _, _ in Tqdm(
+                    self.trainer.datamodule.train_dataloader(),
+                    desc="update_bn",
+                    dynamic_ncols=True,
+                    bar_format=TQDMProgressBar.BAR_FORMAT,
+                ):
+                    yield {"x1": x1.to(self.device), "x2": x2.to(self.device)}
+
+            forward_ = self.ema_model.forward
+            self.ema_model.forward = lambda x: forward_(**x)
+            with self.trainer.precision_plugin.train_step_context():
+                update_bn(data_loader(), self.ema_model)
+            del self.ema_model.forward
 
     def validation_step(self, batch, batch_idx):
         features1, features2, move, result, value = batch
@@ -229,6 +277,27 @@ class Model(pl.LightningModule):
         for key, val in self.validation_step_outputs.items():
             self.log(key, torch.stack(val).mean())
             val.clear()
+
+    def on_test_start(self):
+        if self.hparams.use_ema:
+            self.tmp_model = self.model
+            self.model = self.ema_model
+        return super().on_test_start()
+
+    def test_step(self, batch, batch_idx):
+        self.validation_step(batch, batch_idx)
+
+    def on_test_epoch_end(self):
+        for key, val in self.validation_step_outputs.items():
+            key = "test" + key[3:]
+            self.log(key, torch.stack(val).mean())
+            val.clear()
+
+    def on_test_end(self):
+        super().on_test_end()
+        if self.hparams.use_ema:
+            self.model = self.tmp_model
+            del self.tmp_model
 
 
 def main():
