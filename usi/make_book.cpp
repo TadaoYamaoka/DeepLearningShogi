eb7fb4db03bd7ef1aff1d7cdbf6bd686c0fab2a5
TadaoYamaoka <tadaoyamaoka@gmail.com>
2024-04-29 13:39:39 +0900

    remove max_value

--- usi/make_book.cpp
+++ usi/make_book.cpp
@@ -242,7 +242,6 @@ Score book_search(Position& pos, const std::unordered_map<Key, std::vector<BookE
 		return score;
 	}
 	const auto& entries = itr->second;
-	Score max_value = -ScoreInfinite;
 
 	// MinMaxの探索順に使用する定跡
 	auto itr_best = bookMapBest.end();
@@ -278,7 +277,6 @@ Score book_search(Position& pos, const std::unordered_map<Key, std::vector<BookE
 					searched[key] = { pos.gamePly(), value, beta };
 					return -value;
 				}
-				max_value = std::max(max_value, value);
 			}
 		}
 	}
@@ -336,7 +334,6 @@ Score book_search(Position& pos, const std::unordered_map<Key, std::vector<BookE
 			//}
 			return -value;
 		}
-		max_value = std::max(max_value, value);
 	}
 	for (MoveList<LegalAll> ml(pos); !ml.end(); ++ml) {
 		const Move& move = ml.move();
@@ -387,12 +384,11 @@ Score book_search(Position& pos, const std::unordered_map<Key, std::vector<BookE
 			//}
 			return -value;
 		}
-		max_value = std::max(max_value, value);
 	}
 	//if (std::abs(value) == 71) print_debug_moves(value);
 
 	// βカットされなかった場合はsearchedに追加しない
-	return -max_value;
+	return -alpha;
 }
 
 std::tuple<int, Move, Score> select_best_book_entry(Position& pos, const std::unordered_map<Key, std::vector<BookEntry> >& outMap, const std::vector<BookEntry>& entries, const std::vector<Move>& moves, const std::unordered_map<Key, std::vector<BookEntry> >& bookMapBest) {
