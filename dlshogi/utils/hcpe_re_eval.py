c82d7f35febe0c77c077928afa79e02bbe9a1545
TadaoYamaoka <tadaoyamaoka@gmail.com>
2024-12-28 11:54:28 +0900

    詰み局面をスキップする処理のバグ修正

--- dlshogi/utils/hcpe_re_eval.py
+++ dlshogi/utils/hcpe_re_eval.py
@@ -25,19 +25,18 @@ session = onnxruntime.InferenceSession(args.model, providers=['TensorrtExecution
 x1 = np.empty((batch_size, FEATURES1_NUM, 9, 9), dtype=np.float32)
 x2 = np.empty((batch_size, FEATURES2_NUM, 9, 9), dtype=np.float32)
 
-def evaluate_and_write(i, j):
+def evaluate_and_write(indexes, copy_indexes, start_index, end_index):
     io_binding = session.io_binding()
-    io_binding.bind_cpu_input('input1', x1)
-    io_binding.bind_cpu_input('input2', x2)
+    io_binding.bind_cpu_input('input1', x1[:len(indexes)])
+    io_binding.bind_cpu_input('input2', x2[:len(indexes)])
     io_binding.bind_output('output_policy')
     io_binding.bind_output('output_value')
     session.run_with_iobinding(io_binding)
     _, values = io_binding.copy_outputs_to_cpu()
 
     scores = value_to_score(values.reshape(-1), a)
-    out_hcpes = hcpes[i - j:i + 1].copy()
-    for out_hcpe, score in zip(out_hcpes, scores):
-        out_hcpe['eval'] = score
+    out_hcpes = hcpes[start_index:end_index + 1].copy()
+    out_hcpes['eval'][[i - start_index for i in indexes]] = scores
     out_hcpes.tofile(f_out)
 
 def value_to_score(values, a):
@@ -50,17 +49,24 @@ def value_to_score(values, a):
 
 board = Board()
 j = 0
+indexes = []
+copy_indexes = []
+start_index = 0
 for i in tqdm(range(hcpes_size)):
     if abs(hcpes[i]['eval']) >= 30000:
-        hcpes[i].tofile(f_out)
+        copy_indexes.append(i)
         if i == hcpes_size - 1:
-            evaluate_and_write(i, j)
+            evaluate_and_write(indexes, copy_indexes, start_index, i)
         continue
     board.set_hcp(hcpes[i]['hcp'])
     make_input_features(board, x1[j], x2[j])
+    indexes.append(i)
 
     if j == batch_size - 1 or i == hcpes_size - 1:
-        evaluate_and_write(i, j)
+        evaluate_and_write(indexes, copy_indexes, start_index, i)
         j = 0
+        indexes.clear()
+        copy_indexes.clear()
+        start_index = i + 1
     else:
         j += 1
