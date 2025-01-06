1e8a47bafbc132d60187752b1c41407be31e3620
TadaoYamaoka <tadaoyamaoka@gmail.com>
2025-01-06 20:32:05 +0900

    use_ampオプション追加

--- dlshogi/utils/hcpe3_cache_re_eval.py
+++ dlshogi/utils/hcpe3_cache_re_eval.py
@@ -17,12 +17,14 @@ parser.add_argument('--limit_candidates', type=int, default=10)
 parser.add_argument('--batch_size', '-b', type=int, default=1024)
 parser.add_argument('--network')
 parser.add_argument('--gpu', '-g', type=int, default=0)
+parser.add_argument('--use_amp', action='store_true')
 args = parser.parse_args()
 
 alpha = args.alpha
 dropoff = args.dropoff
 limit_candidates = args.limit_candidates
 batch_size = args.batch_size
+use_amp = args.use_amp
 
 if args.gpu >= 0:
     device = torch.device(f"cuda:{args.gpu}")
@@ -49,11 +51,12 @@ for i in tqdm(range(0, len(indexes), batch_size)):
         chunk[:chunk_size] = chunk_tmp
 
     x1, x2, t1, t2, value = dataloader.mini_batch(chunk)
-    with torch.no_grad():
-        y1, y2 = model(x1, x2)
+    with torch.cuda.amp.autocast(enabled=use_amp):
+        with torch.no_grad():
+            y1, y2 = model(x1, x2)
 
-    y1 = y1.cpu().numpy()
-    y2 = torch.sigmoid(y2).cpu().numpy()
+            y1 = y1.cpu().numpy()
+            y2 = torch.sigmoid(y2).cpu().numpy()
     if chunk_size < batch_size:
         hcpe3_cache_re_eval(chunk_tmp, y1[:chunk_size], y2[:chunk_size], alpha, dropoff, limit_candidates)
     else:
