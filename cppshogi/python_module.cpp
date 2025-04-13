dadaf0629890b67be3525eb88a19a00b967ed413
TadaoYamaoka <tadaoyamaoka@gmail.com>
2025-04-13 11:38:54 +0900

    temperatureオプション追加

--- cppshogi/python_module.cpp
+++ cppshogi/python_module.cpp
@@ -709,7 +709,8 @@ void __hcpe3_merge_cache(const std::string& file1, const std::string& file2, con
 // 事前にキャッシュがロードされていること
 // alpha: 加重平均の係数
 // dropoff: モデルの推論結果の方策の確率をトップから何%低下までを採用するか
-void __hcpe3_cache_re_eval(const size_t len, char* ndindex, char* ndlogits, char* ndvalue, const float alpha_p, const float alpha_v, const float alpha_r, const float dropoff, const int limit_candidates) {
+// temperature: softmax温度パラメータ
+void __hcpe3_cache_re_eval(const size_t len, char* ndindex, char* ndlogits, char* ndvalue, const float alpha_p, const float alpha_v, const float alpha_r, const float dropoff, const int limit_candidates, const float temperature) {
     unsigned int* index = reinterpret_cast<unsigned int*>(ndindex);
     auto logits = reinterpret_cast<float(*)[9 * 9 * MAX_MOVE_LABEL_NUM]>(ndlogits);
     float* values = reinterpret_cast<float*>(ndvalue);
@@ -717,10 +718,11 @@ void __hcpe3_cache_re_eval(const size_t len, char* ndindex, char* ndlogits, char
     const size_t start_index = trainingData.size();
     trainingData.resize(trainingData.size() + len);
 
-    const auto softmax = [](std::vector<float>& probabilities) {
+    const auto softmax = [](std::vector<float>& probabilities, const float temperature) {
         float max = 0.0f;
         for (int i = 0; i < probabilities.size(); i++) {
             float& x = probabilities[i];
+            x /= temperature;
             if (x > max) {
                 max = x;
             }
@@ -761,7 +763,7 @@ void __hcpe3_cache_re_eval(const size_t len, char* ndindex, char* ndlogits, char
                 legal_moves.emplace_back(move16);
             }
             // softmax
-            softmax(probabilities);
+            softmax(probabilities, temperature);
 
             // 確率でフィルタする
             float threshold = 1.0f / MaxLegalMoves;
