abdcae4c97b61bb328f6dc38689f86580ae75446
TadaoYamaoka <tadaoyamaoka@gmail.com>
2025-06-07 15:24:57 +0900

    flipオプション追加

--- dlshogi/utils/yanebook_to_book.py
+++ dlshogi/utils/yanebook_to_book.py
@@ -8,11 +8,14 @@ parser.add_argument('yanebook')
 parser.add_argument('book')
 parser.add_argument('--score_to_count', action='store_true')
 parser.add_argument('--temperature', type=float, default=10.0)
+parser.add_argument('--flip', action='store_true')
 args = parser.parse_args()
 
 ptn = re.compile(r'^([0-9PLNSGRB][a-i*][0-9][a-i]\+?) ([0-9PLNSGRB][a-i*][0-9][a-i]\+?|none) (-?\d+) (\d+) (\d+)')
 
 board = Board()
+if args.flip:
+    board_flip = Board()
 
 count = 0
 dic = {}
@@ -21,10 +24,17 @@ for line in open(args.yanebook, encoding='utf_8_sig'):
     if line[0] == '#':
         continue
     if line[:4] == "sfen":
-        board.set_sfen(line[5:].strip())
+        sfen = line[5:].strip()
+        board.set_sfen(sfen)
         key = board.book_key()
         entries = []
         dic[key] = entries
+        if args.flip:
+            sfen_flip = rotate_sfen(sfen)
+            board_flip.set_sfen(sfen_flip)
+            key_flip = board_flip.book_key()
+            entries_flip = []
+            dic[key_flip] = entries_flip
     else:
         m = ptn.match(line)
         if m:
@@ -34,6 +44,12 @@ for line in open(args.yanebook, encoding='utf_8_sig'):
                 # move, count, score
                 entries.append((move, m.group(5), m.group(3)))
                 count += 1
+            if args.flip:
+                move_flip = move_rotate(move)
+                if board_flip.is_legal(move_flip):
+                    # move, count, score
+                    entries_flip.append((move_flip, m.group(5), m.group(3)))
+                    count += 1
         else:
             raise
 
